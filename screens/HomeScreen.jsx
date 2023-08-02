ba82261a2284c0bbfc0ed1fcff8f2aa4c99f3bfb
Sammy Njoroge <missemadranya@gmail.com>
2023-08-02 03:03:21 +0300

    HomeScreen_UI

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,86 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Entypo } from '@expo/vector-icons'
+import { Cover } from '../assets'
+import { FontAwesome } from '@expo/vector-icons'
+import { TextInput } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { pressIcons } from '../colors'
+import { ActivityIndicator } from 'react-native'
+import { fetchFeeds } from '../sanity'
 
 const HomeScreen = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setisLoading] = useState(false)
+
+
+  const handleSearchTerm = (text) => {
+    setSearchTerm(text)
+  }
+
+  useEffect (() => {
+    setisLoading(true)
+    try {
+      fetchFeeds().then(res => {
+        console.log(res)
+        setInterval(() => {
+          setisLoading (true)
+        }, 1000)
+      })
+    } catch (error) {
+      console.log(error)
+      setisLoading(false)
+      
+    }
+  }, [])
+
+
   return (
-    <SafeAreaView className='flex-1 mt-7' >
-    <View className="justify-items-center items-center" >
-      <Text>HomeScreen</Text>
+    <SafeAreaView className='flex-1 bg-[#d0ddcf]' >
+    <View className="w-full flex-row mt-5 items-center justify-between px-4 py-2" >
+    <Entypo name="chevron-left" size={35} color={pressIcons} />
+    <Image
+    source={Cover}
+    className='w-12 h-12 rounded-xl'
+    resizeMode='contain'
+    />      
     </View>
+    {/*Search box starts here*/}
+
+    <View className='flex-row items-center justify-between px-4 py-2 w-full space-x-6'>
+      <View className='px-4 py-2 bg-white rounded-xl flex-1 flex-row items-center justify-center space-x-2'>
+      <FontAwesome name="search" size={24} color={pressIcons} />
+      <TextInput className='text-base font-semibold flex-1 py-1 text=[#555]'
+      placeholder="Search Here..."
+      value={searchTerm}
+      onChangeText={handleSearchTerm}
+       />
+      </View>
+      <TouchableOpacity className='w-12 h-12 rounded-xl flex items-center justify-center bg-white' >
+      <Ionicons name="filter" size={24} color={pressIcons} />
+
+      </TouchableOpacity>
+    </View>
+    {/*Search box ends here*/}
+
+    {/*Scrollable Container Starts*/}
+
+    <ScrollView className='flex-1 w-full h-full' >
+      {isLoading ? 
+      <View className='flex-1 h-80 items-center justify-center' >
+        <ActivityIndicator 
+        size={"large"}
+        color={pressIcons}
+         />
+      </View> 
+      
+      : 
+      <></>}
+
+    </ScrollView>
+
+    {/*Scrollable Container Ends*/}
+
     </SafeAreaView>
   )
 }
